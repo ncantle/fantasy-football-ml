c03c9dab16df1052fcf90fc67e05eb4d4df65d93
Noah Antle <noahantle97@gmail.com>
2025-08-08 23:31:10 -0700

    Parameterizing position for model

--- src/modeling/train_models.py
+++ src/modeling/train_models.py
@@ -1,26 +1,30 @@
-from src.modeling.qb_model import qb_model
-from src.modeling.wr_model import wr_model
-from src.modeling.rb_model import rb_model
-from src.modeling.te_model import te_model
+from src.modeling.model import model
 import argparse
 import time
 
-def train_models(season, week):
-    start = time.time()
-
-    qb_model(season, week)
-    wr_model(season, week)
-    rb_model(season, week)
-    te_model(season, week)
-
-    end = time.time()
-    print(f"Training completed in {end - start:.2f} seconds.")
+def train_models(season, week, position=None):
+    if position == None:
+        for position in ['qb', 'wr', 'rb', 'te']:
+            print(f'Training {position} model...')
+            start = time.time()
+            model(season, week, position)
+            end = time.time()
+            print(f"Training of {position} model completed in {end - start:.2f} seconds.")
+            print('')
+            print('')
+    else:
+        print(f'Training {position} model...')
+        start = time.time()
+        model(season, week, position)
+        end = time.time()
+        print(f"Training of {position} model completed in {end - start:.2f} seconds.")
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Train all models for a given season/week.")
     parser.add_argument("--season", type=int, required=True, help="Season year, e.g., 2023")
     parser.add_argument("--week", type=int, required=True, help="Week number, e.g., 8")
+    parser.add_argument("--position", type=str, required=False, help="Train model for specific position (qb, wr, rb, or te)")
     
     args = parser.parse_args()
-    train_models(args.season, args.week)
+    train_models(args.season, args.week, args.position)
